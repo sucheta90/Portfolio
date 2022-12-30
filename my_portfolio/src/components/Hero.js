7281463928340d944f972194e2626968e61d47c7
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2022-12-30 11:59:44 -0500

    fixed the footer position

--- my_portfolio/src/components/Hero.js
+++ my_portfolio/src/components/Hero.js
@@ -1,29 +1,15 @@
-import React from 'react'
-import Contact from './Contact'
-import image from '../images/profilePhoto.png'
-import './Hero.css'
+import React from "react";
+import Contact from "./Contact";
+import image from "../images/profilePhoto.png";
+import "./Hero.css";
 
-
-export default function Hero(){
-    
-   return (
-    
-        
-        <div className='hero--content conatiner-fluid' >
-            
-                
-            <img src={image} className='profile--image img-fluid' alt='my_photo'/>
-            <h2>Hello! I am</h2>
-            <h1 className='main-heading'>Sucheta Mukherjee</h1>
-            <h3 className='main-title'>Frontend Web Developer</h3>
-            
-            
-            
-            
-        
-        </div>
-        
-    
-   )
-    
+export default function Hero() {
+  return (
+    <div className="hero--content conatiner-fluid">
+      <img src={image} className="profile--image img-fluid" alt="my_photo" />
+      <h2>Hello! I am</h2>
+      <h1 className="main-heading">Sucheta Mukherjee</h1>
+      <h3 className="main-title">Frontend Web Developer</h3>
+    </div>
+  );
 }
