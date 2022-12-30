2746b6404ae56dab1c7aeb4a10ad6bc164b76b04
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2022-12-30 17:33:22 -0500

    made changes to the displays for different screen sizes

--- my_portfolio/src/components/About.js
+++ my_portfolio/src/components/About.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
 
+export default function About() {
+  return (
+    <>
+      <div className="container-fluid about-page">
+        <h3 className="page--title"> About Myself</h3>
+        <hr></hr>
+        <section className="aboutMe container-fluid">
+          <p>
+            It is my pleasure to introduce myself as a Frontend web developer
+            from Glen Allen, Virginia. Among the languages I use to build
+            user-friendly web applications and websites are HTML, CSS,
+            JAVASCRIPT and REACT.{" "}
+          </p>
 
-export default function About(){
-    return (
-        <>
-        <div className='container-fluid about-page'>
-            
-            <h3 className='page--title'> About Myself</h3>
-            <hr></hr>
-            <section className='aboutMe container-fluid'>
-                
-                <p>    It is my pleasure to introduce myself as a Frontend web developer from Glen Allen, Virginia. Among the languages I use to build user-friendly web applications and websites are HTML, CSS, JAVASCRIPT and REACT. </p>
-                
-                <p>
-                    Being able to learn new things quickly is part of my interest in learning more and growing my skills. As the technologies I use are constantly evolving, I am learning and improving my skills. My goal is to showcase my knowledge, collaborate, and work as a team in a challenging and rewarding environment. If you would like to get in touch, please do so.
-                </p>
+          <p>
+            Being able to learn new things quickly is part of my interest in
+            learning more and growing my skills. As the technologies I use are
+            constantly evolving, I am learning and improving my skills. My goal
+            is to showcase my knowledge, collaborate, and work as a team in a
+            challenging and rewarding environment. If you would like to get in
+            touch, please do so.
+          </p>
 
-                <p>
-                    My previous work experience in the Customer Service industry primarily involved problem solving. After moving into the USA, I had worked as an office aministrative assistant, which helped me understand the key aspects of organizational skills, time management, communication skills, quick learning, critical thinking and last but not the least, attention to details.
-                </p>
-            </section>
-        </div>
+          <p>
+            My previous work experience in the Customer Service industry
+            primarily involved problem solving. After moving into the USA, I had
+            worked as an office aministrative assistant, which helped me
+            understand the key aspects of organizational skills, time
+            management, communication skills, quick learning, critical thinking
+            and last but not the least, attention to details.
+          </p>
+        </section>
+      </div>
     </>
-    )
-    
+  );
 }
--- my_portfolio/src/components/Footer.js
+++ my_portfolio/src/components/Footer.js
@@ -1,16 +1,13 @@
-import React from 'react'
-import Contact from './Contact'
-import "./Footer.css"
+import React from "react";
+import Contact from "./Contact";
+import "./Footer.css";
 
-export default function Footer(){
-    return(
-    
-        
-        <div className='footer container-fluid'> 
-            <Contact/>
-            <p id='footer-copyright'>&copy; Developed by Sucheta Mukherjee in 2022</p>
-            <p id='telephone'>Tel: +1 831-224-7090</p>
-        </div>
-        
-    )
-} 
+export default function Footer() {
+  return (
+    <div className="footer container-fluid">
+      <Contact />
+      <p id="footer-copyright">&copy; Developed by Sucheta Mukherjee </p>
+      <p id="telephone">Tel: +1 831-224-7090</p>
+    </div>
+  );
+}
