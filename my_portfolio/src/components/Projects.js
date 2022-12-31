d8a7d4458a0f143e830912800b8388cc727f5152
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2022-12-30 19:14:37 -0500

    changed the project image of the Tic Tac Toe game

--- my_portfolio/src/components/Projects.js
+++ my_portfolio/src/components/Projects.js
@@ -1,59 +1,41 @@
 import React from "react";
-import "./Projects.css"
+import "./Projects.css";
 import projectData from "../project_components/projectData";
 import ProjectCards from "../project_components/ProjectCards";
 import Skills from "./Skills";
-import BusinessCard from "../images/digi_card.png"
-import SurveyForm from "../images/SurveyForm_snippet.png"
-import PortfolioSnippet from "../images/portfolioSnippet.png"
-import TicTacToe from "../images/tic_tac_toe.png"
+import BusinessCard from "../images/digi_card.png";
+import SurveyForm from "../images/SurveyForm_snippet.png";
+import PortfolioSnippet from "../images/portfolioSnippet.png";
+import TicTacToe from "../images/ticTacToe.png";
 
-export default function Projects(){
-   const data = projectData.map(element =>{
-   let image = PortfolioSnippet;
-   if (element.id==="tic_tac_toe"){
-     image = TicTacToe;
-   } else if(element.id==="digital_business_card"){
-     image = BusinessCard;
-   }else if(element.id==="survey_form"){
-     image = SurveyForm;
-   }
+export default function Projects() {
+  const data = projectData.map((element) => {
+    let image = PortfolioSnippet;
+    if (element.id === "ticTacToe") {
+      image = TicTacToe;
+    } else if (element.id === "digital_business_card") {
+      image = BusinessCard;
+    } else if (element.id === "survey_form") {
+      image = SurveyForm;
+    }
     return (
-        <ProjectCards 
-            title= {element.title}
-            image= {image}
-            description = {element.description}
-            liveurl = {element.liveUrl}
-            githuburl = {element.githubUrl}
-            key= {element.title}
-            isDemoAvailable= {element.isDemoAvailable}
-    
-        />
-    )
-   }
+      <ProjectCards
+        title={element.title}
+        image={image}
+        description={element.description}
+        liveurl={element.liveUrl}
+        githuburl={element.githubUrl}
+        key={element.title}
+        isDemoAvailable={element.isDemoAvailable}
+      />
+    );
+  });
 
-   )
-    
-    return (
-        <div className="container-fluid project-page">
-            <h3 className="page--title">PROJECTS</h3>
-            <div className="projects container-fluid">
-                
-                {data}
-                
-            </div>  
-            <Skills />
-        </div>
-    )
+  return (
+    <div className="container-fluid project-page">
+      <h3 className="page--title">PROJECTS</h3>
+      <div className="projects container-fluid">{data}</div>
+      <Skills />
+    </div>
+  );
 }
-
-
-
-
-
-
-
-                
-            
-
-          
--- my_portfolio/src/project_components/projectData.js
+++ my_portfolio/src/project_components/projectData.js
@@ -1,43 +1,44 @@
 // import BusinessCard from ;
-// import SurveyForm from 
-export default[
-    {
-        title: "Tic Tac Toe",
-        id: "tic_tac_toe",
-        image: "../images/tic_toc_toe.png",
-        description: "This project is based upon the Form elements, from freeCodeCamp - Responsive Web Design. This project aims at better understanding of the HTML Form element that includes different input types.",
-        liveUrl: 'https://tictactoebysucheta.netlify.app/',
-        githubUrl: 'https://github.com/sucheta90/Tic_Tac_Toe',
-        isDemoAvailable: true,
-
-    },
-    {
-        title: "Digital Business Card",
-        id: "digital_business_card",
-        image: "../images/digi_card.png",
-        description: "This project is based upon the Form elements, from freeCodeCamp - Responsive Web Design. This project aims at better understanding of the HTML Form element that includes different input types.",
-        liveUrl: 'https://digital-bz-card-sucheta.netlify.app/',
-        githubUrl: 'https://github.com/sucheta90/Digital_business_card',
-        isDemoAvailable: true,
-
-    },
-    {
-        title: "Survey Form",
-        id: "survey_form",
-        image: "../images/SurveyForm_snippet.png",
-        description: 'This project is based upon the Form elements, from freeCodeCamp - Responsive Web Design. This project aims at better understanding of the HTML Form element that includes different input types.',
-        liveUrl:'https://jolly-klepon-0395fa.netlify.app/',
-        githubUrl: 'https://github.com/sucheta90/SurveyForm',
-        isDemoAvailable: true,
-    },
-    {
-        title: 'Portfolio Website',
-        id: "portfolio_website",
-        image: "../images/portfolioSnippet.png",
-        description:'This website is built using React. For navigating between pages React Router has been used. With the help of Bootstrap, the page becomes responsive.',
-        liveUrl:'https://suchetamukherjee.netlify.app',
-        githubUrl:'https://github.com/sucheta90/Portfolio',
-        isDemoAvailable: false,
-    }
-
-]
+// import SurveyForm from
+export default [
+  {
+    title: "Tic Tac Toe",
+    id: "ticTacToe",
+    image: "../images/ticTacToe.png",
+    description:
+      "This project is based upon the Form elements, from freeCodeCamp - Responsive Web Design. This project aims at better understanding of the HTML Form element that includes different input types.",
+    liveUrl: "https://tictactoebysucheta.netlify.app/",
+    githubUrl: "https://github.com/sucheta90/Tic_Tac_Toe",
+    isDemoAvailable: true,
+  },
+  {
+    title: "Digital Business Card",
+    id: "digital_business_card",
+    image: "../images/digi_card.png",
+    description:
+      "This project is based upon the Form elements, from freeCodeCamp - Responsive Web Design. This project aims at better understanding of the HTML Form element that includes different input types.",
+    liveUrl: "https://digital-bz-card-sucheta.netlify.app/",
+    githubUrl: "https://github.com/sucheta90/Digital_business_card",
+    isDemoAvailable: true,
+  },
+  {
+    title: "Survey Form",
+    id: "survey_form",
+    image: "../images/SurveyForm_snippet.png",
+    description:
+      "This project is based upon the Form elements, from freeCodeCamp - Responsive Web Design. This project aims at better understanding of the HTML Form element that includes different input types.",
+    liveUrl: "https://jolly-klepon-0395fa.netlify.app/",
+    githubUrl: "https://github.com/sucheta90/SurveyForm",
+    isDemoAvailable: true,
+  },
+  {
+    title: "Portfolio Website",
+    id: "portfolio_website",
+    image: "../images/portfolioSnippet.png",
+    description:
+      "This website is built using React. For navigating between pages React Router has been used. With the help of Bootstrap, the page becomes responsive.",
+    liveUrl: "https://suchetamukherjee.netlify.app",
+    githubUrl: "https://github.com/sucheta90/Portfolio",
+    isDemoAvailable: false,
+  },
+];
